c5a40299787e54f02cc4c5cf05616576abcfe522
orlando-lam <orlando.lam@kellogg.northwestern.edu>
2022-04-18 16:28:19 +0000

    exercise

--- practice-2.sql
+++ practice-2.sql
@@ -19,4 +19,9 @@
 -- | https://www.airbnb.com/rooms/25367014 | 16           |
 -- | https://www.airbnb.com/rooms/25682427 | 16           |
 
+.mode columns
+.headers on
 
+SELECT url, accommodates
+FROM listings
+WHERE accommodates = 16;
